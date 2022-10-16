eda4617c1bc3e951ad77bd52fd6123e054ae882b
Kristina Gromova <christina28romova@yandex.ru>
2022-10-16 22:23:04 +0300

    Updates

--- src/components/ui/ButtonCart/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Styles from './styles.module.css';
-
-
-export const ButtonCart = ({onClick}) => {
-  return (
-    <button className={Styles.button} onClick={onClick} />
-  )
-}
